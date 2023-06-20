5f9052f946db7286e3189dc73e48c8e72a1a64c6
P. Ricardo <pauloricardo.jesus2011@gmail.com>
2023-06-20 13:26:09 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculo() {
+    const V = parseInt(document.getElementById('V').value);
+    const N = parseInt(document.getElementById('N').value);
+    const multiplicador = Math.floor(N/5)
+    let T;
+
+    const resultado = document.getElementById('resultado');
+
+    T = (5+V)*multiplicador+V+20
+
+    console.log('Valor de N é: ' + N);
+    console.log('Valor de V é: ' + V);
+    resultado.innerText = `Seu resultado deu ${T}`;
+}
